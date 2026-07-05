243a2de2448a79f0f6ce53f28579e00b4661b7a6
yunusabdul38 <yunusabdulmajidyunus38@gmail.com>
2026-07-05 18:50:18 +0100

    01-05

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,3 @@
 # Create a native segwit address and get the public key from the address.
+SEGWIT_ADDRESS=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress "bech32")
+bitcoin-cli -regtest -rpcwallet=builderswallet  getaddressinfo $SEGWIT_ADDRESS | jq ".pubkey"
